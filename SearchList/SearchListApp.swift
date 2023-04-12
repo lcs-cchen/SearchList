b15b41f0af1f59d0a9d6f8f4d810ccc01be2eea0
cyrus chen <pschen@lcs.on.ca>
2023-04-12 11:45:10 -0400

    Initial Commit

---
+++ SearchList/SearchListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchListApp.swift
+//  SearchList
+//
+//  Created by Cyrus Chen on 12/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct SearchListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
