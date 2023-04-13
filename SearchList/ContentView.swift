f6501337d0d8ec899d2d6c04568662e03cc22119
cyrus chen <pschen@lcs.on.ca>
2023-04-12 21:50:22 -0400

    Finish base app

--- SearchList/ContentView.swift
+++ SearchList/ContentView.swift
@@ -8,17 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    
+    //MARK: stored propreties
+    @State var items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5",]
+    
+    @State var searchText = ""
+    
+    //MARK: Computed propreties
+    var filteredItems: [String]{
+        
+        if searchText.isEmpty {
+            return items
+            
+        }else{
+            var matchingItems: [String] = []
+            
+            for item in items {
+                if item.lowercased().contains(searchText.lowercased()) {
+                    matchingItems.append(item)
+                }
+            }
+            return matchingItems
         }
-        .padding()
+    }
+    var body: some View{
+        
+        NavigationView{
+            
+            VStack{
+                
+                Text("Searching on: \(searchText)")
+                
+                List(filteredItems, id: \.self) {
+                    currentItem in
+                    Text(currentItem)
+                }
+                .searchable(text: $searchText)
+            }
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
